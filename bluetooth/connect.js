936f87d356e2b5302923cc395f2397fe4272166c
Sarah GP <sarah.groff.palermo@gmail.com>
2015-01-30 15:38:58 -0500

    Stopping on pubnub for now; very broken

---
+++ bluetooth/connect.js
@@ -0,0 +1,13 @@
+var pubnub = require('pubnub').init({
+  publish_key: 'pub-c-c68b82e8-a483-44c6-9efb-29ee321615df',
+  subscribe_key: 'sub-c-fc32499e-a8b4-11e4-bf84-0619f8945a4f'
+});
+
+setInterval(function(){
+  pubnub.publish({
+  channel: "book-message",
+  error: function(e){console.log("Pubnub error")},
+  message: 'hi',
+  callback: function(){console.log('Msg sent')}
+});
+}, 1000);
