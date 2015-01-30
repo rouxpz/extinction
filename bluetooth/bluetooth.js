e92ee5c09a7bdf23298f47d5eaf715658c7a01a5
Sarah GP <sarah.groff.palermo@gmail.com>
2015-01-30 16:17:19 -0500

    Maybe working?

--- bluetooth/bluetooth.js
+++ bluetooth/bluetooth.js
@@ -6,18 +6,18 @@ var counter = 0;
 
 ble.on('ready', function(err) {
 
-  // var pubnub = require('pubnub').init({
-  //   publish_key: 'pub-c-c68b82e8-a483-44c6-9efb-29ee321615df',
-  //   subscribe_key: 'sub-c-fc32499e-a8b4-11e4-bf84-0619f8945a4f'
-  // });
+  var pubnub = require('pubnub').init({
+    publish_key: 'pub-c-c68b82e8-a483-44c6-9efb-29ee321615df',
+    subscribe_key: 'sub-c-fc32499e-a8b4-11e4-bf84-0619f8945a4f'
+  });
 
   function countClearSend(){
-    // pubnub.publish({
-    //   channel: "extinction",
-    //   error: function(e){console.log("Pubnub error");},
-    //   message: '' + counter,
-    //   callback: function(){console.log('Count sent', counter);}
-    // });
+    pubnub.publish({
+      channel: "extinction",
+      error: function(e){console.log("Pubnub error");},
+      message: '' + counter,
+      callback: function(){console.log('Count sent', counter);}
+    });
     
     console.log('Counter', counter);
     counter = 0; // Reset counter so next 5s takes in new data
